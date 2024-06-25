fe46d67f31683c2fce58ab61eafe38dc0c77d228
Dmitri Rein <Dmitri.Rein@ibm.com>
2024-06-25 14:59:08 +0200

    Update Weather_API.py
    .

--- Weather_API.py
+++ Weather_API.py
@@ -1,1 +1,18 @@
-https://api.openweathermap.org/data/2.5/onecall?lat=47.376888&lon=8.541694&appid=6cba7b191454e2da0f71bd1a674b32db
+import requests
+
+#API_Key = "6cba7b191454e2da0f71bd1a674b32db"
+
+#lat = 47.376888
+#lon = 8.541694
+
+URL = "https://api.openweathermap.org/data/2.5/onecall?lat="+47.376888+"&lon="+ 8.541694 +"&appid=6cba7b191454e2da0f71bd1a674b32db"
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    daten = response.json()
+    
+else:
+    print ("Es gab leider einen Fehler!")
+
+
