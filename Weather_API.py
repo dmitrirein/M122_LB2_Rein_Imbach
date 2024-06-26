f9dfb1285dee389336ed511deb28940f26f8220e
Dmitri Rein <Dmitri.Rein@ibm.com>
2024-06-26 09:48:41 +0200

    Update Weather_API.py

--- Weather_API.py
+++ Weather_API.py
@@ -5,13 +5,13 @@ import requests
 #lat = 47.376888
 #lon = 8.541694
 
-URL = "https://api.openweathermap.org/data/2.5/onecall?lat="+47.376888+"&lon="+ 8.541694 +"&appid=6cba7b191454e2da0f71bd1a674b32db"
+URL = "https://api.openweathermap.org/data/2.5/forecast?lat=47.376888&lon=8.541694&appid=6cba7b191454e2da0f71bd1a674b32db"
 
 response = requests.get(URL)
 
 if response.status_code == 200:
     daten = response.json()
-    
+
 else:
     print ("Es gab leider einen Fehler!")
 
