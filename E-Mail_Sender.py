1be0ed0265e365ddac2942b57d381ab9e0db5cdd
dmitrirein <dmitri.rein@edu.tbz.ch>
2024-07-11 20:18:29 +0200

    c

--- E-Mail_Sender.py
+++ E-Mail_Sender.py
@@ -5,45 +5,45 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-# Define email sender and receiver
+# Absender- und Empfänger-E-Mail-Adressen definieren
 email_sender = 'dmitrirein545@gmail.com'
 email_password = AppPassword
 email_receiver = ["dmitri.rein@edu.tbz.ch", "dmitrirein545@gmail.com", "Daniel.Imbach@edu.tbz.ch"]
 
-# Read the HTML file content
+# HTML-Dateiinhalt lesen
 with open('Temperatur.html', 'r', encoding='utf-8') as file:
     html_content = file.read()
 
-# Create the email subject
+# Betreff der E-Mail erstellen
 subject = 'Wetter der nächsten 5 Tagen'
 
-# Create a multipart message and set headers
+# Multipart-Nachricht erstellen und Header setzen
 message = MIMEMultipart()
 message['From'] = email_sender
 message['To'] = ", ".join(email_receiver)
 message['Subject'] = subject
 
-# Attach the HTML message body
+# HTML-Nachrichtentext anhängen
 message.attach(MIMEText(html_content, 'html'))
 
-# Open the PDF file in binary mode
+# PDF-Datei im Binärmodus öffnen
 with open("Wetter.pdf", "rb") as file:
     part = MIMEBase('application', 'octet-stream')
     part.set_payload(file.read())
 
-# Encode the file in ASCII characters to send by email    
+# Datei in ASCII-Zeichen kodieren, um sie per E-Mail zu senden
 encoders.encode_base64(part)
 
-# Add header as key/value pair to attachment part
+# Header als Schlüssel/Wert-Paar zum Anhangsteil hinzufügen
 part.add_header('Content-Disposition', 'attachment; filename="Wetter.pdf"')
 
-# Attach the file to the message
+# Datei zur Nachricht hinzufügen
 message.attach(part)
 
-# Connect to the Gmail SMTP server
+# Verbindung zum Gmail-SMTP-Server herstellen
 with smtplib.SMTP('smtp.gmail.com', 587) as server:
-    server.starttls()  # Secure the connection
-    server.login(email_sender, email_password)  # Log in to the email account
-    server.sendmail(email_sender, email_receiver, message.as_string())  # Send the email
+    server.starttls()  # Verbindung sichern
+    server.login(email_sender, email_password)  # Beim E-Mail-Konto anmelden
+    server.sendmail(email_sender, email_receiver, message.as_string())  # E-Mail senden
 
-print("Email sent successfully!")
+print("E-Mail wurde erfolgreich gesendet!")
--- Weather_API.py
+++ Weather_API.py
@@ -2,26 +2,27 @@ import requests
 from html_content import html_skript
 from collections import Counter
 from fpdf import FPDF
-import os 
 
+# Breitengrad und Längengrad für Zürich, Schweiz
+lat = 47.376888
+lon = 8.541694
 
-API_Key = os.getenv("api_key")
+# URL für die OpenWeatherMap API mit den angegebenen Koordinaten und API-Schlüssel
+URL = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=6cba7b191454e2da0f71bd1a674b32db"
 
-#lat = 47.376888
-#lon = 8.541694
-
-URL = f"https://api.openweathermap.org/data/2.5/forecast?lat=47.376888&lon=8.541694&appid=6cba7b191454e2da0f71bd1a674b32db" 
-
+# Anfrage an die API senden
 response = requests.get(URL)
 
+# Überprüfen, ob die Anfrage erfolgreich war
 if response.status_code == 200:
-    wetter_daten = response.json()
+    wetter_daten = response.json()  # JSON-Daten der Antwort extrahieren
 else:
-    print ("Es gab leider einen Fehler!")
-    
-    
+    print("Es gab leider einen Fehler!")  # Fehlernachricht ausgeben
 
+# Temperaturen in Celsius umwandeln und abrunden
 temperaturen = [round(elem["main"]["temp_max"] - 273.15, 1) for elem in wetter_daten["list"]]
+
+# Datum extrahieren und sortieren
 daten = [elem["dt_txt"].split(" ")[0] for elem in wetter_daten["list"]]
 daten = list(set(daten))
 daten.sort()
@@ -32,7 +33,7 @@ for datum in daten:
     tag_temperaturen = [round(elem["main"]["temp_max"] - 273.15, 1) for elem in wetter_daten["list"] if elem["dt_txt"].split(" ")[0] == datum]
     max_temp_pro_tag[datum] = max(tag_temperaturen)
 
-
+# HTML-Skript mit den Daten füllen
 for datum, temp in max_temp_pro_tag.items():
     html_skript += f"""
         <tr>
@@ -53,39 +54,40 @@ with open("Temperatur.html", "w", encoding="utf-8") as file:
 
 print("HTML-Datei wurde erfolgreich erstellt: Temperatur.html")
 
+# Informationen für die PDF-Datei sammeln
 PDF_Infos = {}
 for datum in daten:
     tag_info = {}
 
+    # Maximal- und Minimaltemperaturen des Tages sammeln
     max_tag_temperaturen = [round(elem["main"]["temp_max"] - 273.15, 1) for elem in wetter_daten["list"] if elem["dt_txt"].split(" ")[0] == datum]
     tag_info.update({"temp_max": max(max_tag_temperaturen)})
 
     min_tag_temperaturen = [round(elem["main"]["temp_min"] - 273.15, 1) for elem in wetter_daten["list"] if elem["dt_txt"].split(" ")[0] == datum]
     tag_info.update({"temp_min": min(min_tag_temperaturen)})
 
+    # Häufigste Wetterkonditionen des Tages sammeln
     Wetter = [elem["weather"][0]["main"] for elem in wetter_daten["list"] if elem["dt_txt"].split(" ")[0] == datum]
     Wetter = Counter(Wetter).most_common(1)[0][0]
     tag_info.update({"weather": Wetter})
- 
+
+    # Beschreibung der häufigsten Wetterkonditionen des Tages sammeln
     Wetter_Description = [elem["weather"][0]["description"] for elem in wetter_daten["list"] if elem["dt_txt"].split(" ")[0] == datum]
     Wetter_Description = Counter(Wetter_Description).most_common(1)[0][0]
     tag_info.update({"description": Wetter_Description})
 
-
     PDF_Infos[datum] = tag_info
 
-
-
-
+# Funktion, um ein Dictionary in eine PDF-Datei zu schreiben
 def dict_to_pdf(data, file_name):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size = 12)
+    pdf.set_font("Arial", size=12)
     
     for key, value in data.items():
-        pdf.cell(200, 10, txt = f"{key}: {value}", ln = True)
+        pdf.cell(200, 10, txt=f"{key}: {value}", ln=True)
     
     pdf.output(file_name)
 
-
+# PDF-Datei erstellen
 dict_to_pdf(PDF_Infos, "Wetter.pdf")
